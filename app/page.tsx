2922d50c068fb667b4fc72e2ac7d8810fc27d8a4
Vahid Jafari <vahid.jafaro.pro@gmail.com>
2025-11-22 17:29:28 +0330

    Importing LightRays

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 const Page = () => {
-  return <div>Welcome to Next.js 16!</div>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can&apos;t Miss
+      </h1>
+      <p>Hackathons, Meetups, and Conferences, All in One Place.</p>
+    </section>
+  );
 };
 
 export default Page;
